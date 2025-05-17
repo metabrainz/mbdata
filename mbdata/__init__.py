429a4855d0b782a43e2e1cab87e673c52a2cbb58
Kartik Ohri <kartikohri13@gmail.com>
2025-05-17 14:09:14 +0530

    Convert project to PEP 621 metadata and enable dynamic versioning
    Transitioned from Poetry-specific metadata to PEP 621 standard using `[project]` and
    added `poetry-dynamic-versioning` for dynamic version management using git tags.
    
    The `poetry-dynamic-versioning` needs to be installed manually during development by
    running `poetry self add 'poetry-dynamic-versioning[plugin]'`.
    
    References for dynamic versioning with poetry:
    - https://python-poetry.org/docs/pyproject/#version
    - https://github.com/mtkennerly/poetry-dynamic-versioning?tab=readme-ov-file#installation

--- mbdata/__init__.py
+++ mbdata/__init__.py
@@ -1,4 +1,6 @@
 # Copyright (C) 2013 Lukas Lalinsky
 # Distributed under the MIT license, see the LICENSE file for details.
 
-__version__ = "30.0.0"
+# automatically filled in and updated by poetry-dynamic-versioning plugin
+# in distributions
+__version__ = "0.0.0"
